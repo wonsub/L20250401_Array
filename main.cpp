50c54741ee4296ddc557cd451e75576799162977
wonsub <secretfice@naver.com>
2025-04-01 16:37:07 +0900

    별모양 추가

--- main.cpp
+++ main.cpp
@@ -5,44 +5,17 @@ using namespace std;
 
 int main()
 {
-	int PlayerX = 1;
-	int PlayerY = 1;
 
-	
-	int map[10][10] = 
-	{
-		{ 1,1,1,1,1,1,1,1,1,1 },
-		{ 1,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,1 },
-		{ 1,1,1,1,1,1,1,1,1,1 }
-	};
+	int MaxCount = 0;
 
+	cin >> MaxCount;
 
-
-	//PlayerX++;
-	//for (int y = 0; y < 10; y++)
-	//{
-	//		
-	//	PlayerX++;
-	//	for (int x = 0; x < 10; x++)
-	//	{
-	//			
-	//		{
-	//			cout << map[y][x];
-	//		}
-	//	}
-	//	cout << endl;
-	//}
-
-
-	
-	for (int j = 0; j < 5; j++)
+	/**
+	**
+	***
+	****
+	******/
+	for (int j = 0; j < MaxCount; j++)
 	{
 		for (int i = 0; i <= j; i++)
 		{
@@ -54,15 +27,50 @@ int main()
 
 	cout << endl;
 
-	for (int j = 5; j > 0; j--)
+
+
+	//*****		
+	//****
+	//***
+	//**
+	//*
+
+	for (int j = 0; j <MaxCount; j++)
 	{
-		for (int i = 0; i < j; i++)
+		for (int i = 0; i < MaxCount -j; i++)
 		{
 
 			cout << "*";
 		}
 		cout << endl;
 	}
+
+	cout << endl;
+
+
+
+	//*****
+	// ****
+	//  ***
+	//   **
+	//	  *
+	for (int j = 0; j < MaxCount; j++)
+	{
+		for (int i = 0; i < j; i++)
+		{
+
+			cout << " ";
+		}
+		for (int i = 0; i < MaxCount - j; i++)
+		{
+			cout << "*";
+			
+		}
+	
+		cout << endl;
+	}
+
+
 
 
 	
